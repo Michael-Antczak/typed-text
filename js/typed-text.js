32a892cc30a6c323f4a18236796532482fa98e69
Michael Antczak <Michael.Antczak@gmail.com>
2017-04-19 15:31:12 +0100

    Initial commit
    Also added the scheleton for the whole script.

---
+++ js/typed-text.js
@@ -0,0 +1,29 @@
+(function() {
+    
+    // Settings for the script: speed, etc. 
+
+
+    // Find a get the element with id ="typed-text"
+
+
+    // Hide the text
+
+
+    // Split text into single characters
+    // Wrap into span element
+    // Add default class "no-seen" to hide characters (will use "seen" class too?)
+
+
+    // Position the cursor at the beginning
+
+
+    // Display the cursor & flashing
+
+
+    // Iterate over the letters
+    //  - for each 
+    //      - move cursor 1 span element to the right
+    //      - remove class "no-seen"  (any other effects?)
+
+    
+})();
